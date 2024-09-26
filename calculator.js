9ec02da3ff142faee30fe769d3d0ada3347a92f3
rajagopalreddy-tm <rajagopoal.reddy@trainingmug.com>
2024-09-26 17:37:46 +0530

    chore: calculator app

---
+++ calculator.js
@@ -0,0 +1,17 @@
+process.stdin.on('data',(chunk) => {
+  const input = chunk.toString().trim();
+  if (input === 'quit'){
+    process.exit(0);
+  }
+  try {
+    const value = eval(input);
+    console.log(`${value}`);
+  } catch (exception) {
+    console.log("i dont know how to do that");
+  }
+
+  process.stdout.write('Enter a simple equation: ');
+})
+
+
+process.stdout.write('Enter a simple equation: ');
